2aa366bb2c4a8b3afb831f0bc243053c42c7cf02
whalemare <nanostet@gmail.com>
2019-06-04 20:57:43 +0700

    Update library

--- app/src/main/java/jetray/tictactoe/SplashScreen.java
+++ app/src/main/java/jetray/tictactoe/SplashScreen.java
@@ -1,14 +1,12 @@
 package jetray.tictactoe;
 
 import android.content.Intent;
-import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -17,14 +15,10 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow(); // in Activity's onCreate() for instance
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -34,6 +28,5 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         }, 4000);
-
     }
 }
